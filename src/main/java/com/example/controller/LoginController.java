d410a075146531b2ba8a0914bb9999099b164632
Tsubakurouuu <yzpolice2896@icloud.com>
2023-05-12 18:47:56 +0900

    [Create]ログイン画面、コントローラ、共通ヘッダー

---
+++ src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	//ログインページに遷移するための処理
+	@GetMapping("/login")
+	public String getLogin() {
+		//home/login.htmlを呼び出す
+		return "login/login";
+	}
+}
