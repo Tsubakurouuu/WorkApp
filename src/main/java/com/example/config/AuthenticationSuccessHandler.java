36d8fdfc84041c75a2f2c7834abf03bcbde9c5a1
Tsubakurouuu <yzpolice2896@icloud.com>
2023-05-20 08:57:49 +0900

    [Add]各処理へのコメント文

--- src/main/java/com/example/config/AuthenticationSuccessHandler.java
+++ src/main/java/com/example/config/AuthenticationSuccessHandler.java
@@ -12,12 +12,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationSuccessHandler implements org.springframework.security.web.authentication.AuthenticationSuccessHandler {
-
+	
+	//ログイン成功時に呼び出される処理
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			org.springframework.security.core.Authentication authentication) throws IOException, ServletException {
+		//GrantedAuthority オブジェクトの配列をセットに変換する
 		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-
+		
+		//権限ごとでログイン後の遷移先を変えるif文
         if (roles.contains("ADMIN")) {
         	response.sendRedirect(request.getContextPath() + "/admin/users");
         }else if(roles.contains("USER")){
