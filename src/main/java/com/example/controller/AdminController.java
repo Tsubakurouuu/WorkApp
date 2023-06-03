cd77f2abd0c4fc8c5e68f9e87e183f24af8be99d
Tsubakurouuu <yzpolice2896@icloud.com>
2023-06-03 19:21:37 +0900

    [Update]ユーザー一覧画面,ユーザー名を押下した時の遷移先,html

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -48,7 +48,16 @@ public class AdminController {
 	
 	//ユーザー一覧画面に遷移するための処理
 	@GetMapping("/users")
-	public String getAdminUserIndex(Model model) {
+	public String getAdminUserIndex(Model model, Integer year, Integer month) {
+		//カレンダークラスのオブジェクトを生成
+		Calendar calendar = Calendar.getInstance();
+		//現在の年を取得
+        year = calendar.get(Calendar.YEAR);
+        //現在の月を取得
+        month = calendar.get(Calendar.MONTH) + 1;
+        //Modelに登録
+        model.addAttribute("year", year);
+		model.addAttribute("month", month);	
 		//ユーザー一覧取得
 		List<MUser> userList = userService.getUserList();
 		//Modelに登録
