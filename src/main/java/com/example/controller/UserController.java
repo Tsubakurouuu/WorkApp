cfb363267549c2d28c6d209190093ba7cf5f0abe
Tsubakurouuu <yzpolice2896@icloud.com>
2023-06-05 22:09:05 +0900

    [Update]出退勤申請画面,バリデーション付与,controller,form,html

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -9,12 +9,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.form.GroupOrder;
 import com.example.form.RequestFormForm;
 import com.example.model.MUser;
 import com.example.model.RequestForm;
@@ -52,7 +55,16 @@ public class UserController {
 	
 	//確認画面へボタン押下時の処理
 	@PostMapping("/form/confirm")
-	public String postUserFormConfirm(@ModelAttribute RequestFormForm form) {
+	public String postUserFormConfirm(@ModelAttribute @Validated(GroupOrder.class) RequestFormForm form, BindingResult bindingResult, Integer id, Model model) {
+		//入力チェック結果
+		if(bindingResult.hasErrors()) {
+			//勤怠情報取得
+			Work workDetail = workService.selectWork(id);
+			//Modelに登録
+			model.addAttribute("workDetail", workDetail);
+			//NGがあれば出退勤申請画面に戻る
+			return "user/form";
+		}
 		//user/form_confirm.htmlを呼び出す
 		return "user/form_confirm";
 	}
--- src/main/java/com/example/form/RequestFormForm.java
+++ src/main/java/com/example/form/RequestFormForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.example.model.Work;
 
 import lombok.Data;
@@ -12,20 +17,41 @@ public class RequestFormForm {
 	private Integer workId;
 	//出勤ステータス
 	private Integer workStatus;
+	
 	//出勤(時)
+	@Min(value = 0, groups = ValidGroup1.class)
+	@Max(value = 23, groups = ValidGroup1.class)
 	private Integer attendanceHour;
+	
 	//出勤(分)
+	@Min(value = 0, groups = ValidGroup1.class)
+	@Max(value = 59, groups = ValidGroup1.class)
 	private Integer attendanceMinute;
+	
 	//退勤(時)
+	@Min(value = 0, groups = ValidGroup1.class)
+	@Max(value = 23, groups = ValidGroup1.class)
 	private Integer leavingHour;
+	
 	//退勤(分)
+	@Min(value = 0, groups = ValidGroup1.class)
+	@Max(value = 59, groups = ValidGroup1.class)
 	private Integer leavingMinute;
+	
 	//休憩(時)
+	@Min(value = 0, groups = ValidGroup1.class)
+	@Max(value = 23, groups = ValidGroup1.class)
 	private Integer restHour;
+	
 	//休憩(分)
+	@Min(value = 0, groups = ValidGroup1.class)
+	@Max(value = 59, groups = ValidGroup1.class)
 	private Integer restMinute;
+	
 	//申請理由
+	@Length(max = 100, groups = ValidGroup1.class)
 	private String comment;
+	
 	//勤怠テーブル
 	private Work work;
 }
