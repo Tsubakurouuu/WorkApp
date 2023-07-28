38f1abf325ef08d6aa7b050ff4f7eb033bdcb473
Tsubakurouuu <yzpolice2896@icloud.com>
2023-07-28 23:28:40 +0900

    [Add]testInsertUser()メソッドの追加,test

--- src/test/java/com/example/service/impl/MUserServiceImplTest.java
+++ src/test/java/com/example/service/impl/MUserServiceImplTest.java
@@ -1,17 +1,22 @@
 package com.example.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.model.MUser;
 import com.example.repository.MUserMapper;
@@ -22,6 +27,9 @@ public class MUserServiceImplTest {
 
 	@MockBean
 	private MUserMapper mapper;
+	
+	@MockBean
+    private PasswordEncoder encoder;
 	
 	@Autowired
 	private MUserService userService;
@@ -35,7 +43,7 @@ public class MUserServiceImplTest {
 		mockUserList.add(new MUser());
 		//mapperのselectUserListメソッドが呼び出されたときにモックのMUserリストを返すように設定
 		when(mapper.selectUserList()).thenReturn(mockUserList);
-		//テスト対象のメソッドを呼び出して結果を受け取る
+		//テスト対象のメソッドを実行してMUserのListを受け取る
 		List<MUser> resultUserList = userService.selectUserList();
 		//モックのMUserリストが返されることを確認
 		assertSame(mockUserList, resultUserList);
@@ -52,15 +60,29 @@ public class MUserServiceImplTest {
 		mockUserDetail.setUserId(testUserId);
 		//mapperのselectUserDetailメソッド呼び出されたときにモックのMUserを返すように設定
 		when(mapper.selectUserDetail(testUserId)).thenReturn(mockUserDetail);
-		//テスト対象のメソッドを呼び出して結果を受け取る
+		//テスト対象のメソッドを実行してMUserを受け取る
 		MUser resultUserDetail = userService.selectUserDetail(testUserId);
 		//モックのMUserが返されることを確認
 		assertSame(mockUserDetail, resultUserDetail);
 	}
 
 	@Test
+	@DisplayName("ユーザー新規登録テスト")
 	public void testInsertUser() {
-		fail("まだ実装されていません");
+		//モックのMUserインスタンスの生成
+		MUser mockUser = new MUser();
+		//モックのMUserに初期パスワードを設定
+		mockUser.setPassword("rawPassword");
+		//パスワードエンコーダーが任意の文字列をエンコードした場合、"encodedPassword"を返すように設定
+		when(encoder.encode(anyString())).thenReturn("encodedPassword");
+		//テスト対象のメソッドを実行してMUserを登録する
+		userService.insertUser(mockUser);
+		//mapperのinsertUserが1度だけ呼び出されたことを確認
+		verify(mapper, times(1)).insertUser(mockUser);
+		//PasswordEncoderのencodeが1度だけ呼び出されたことを確認
+		verify(encoder, times(1)).encode("rawPassword");
+		//モックMUserのパスワードが正しくエンコードされたことを確認
+        assertEquals("encodedPassword", mockUser.getPassword());
 	}
 
 	@Test
