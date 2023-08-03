c8973a176d1cf56d47c77884163ee321b83c4bf9
Tsubakurouuu <yzpolice2896@icloud.com>
2023-08-04 01:03:56 +0900

    [Create]testLoadUserByUsername()メソッドの作成,test

---
+++ src/test/java/com/example/service/impl/UserDetailsServiceImplTest.java
@@ -0,0 +1,61 @@
+package com.example.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.model.MUser;
+import com.example.service.MUserService;
+
+@ExtendWith(MockitoExtension.class)
+class UserDetailsServiceImplTest {
+	
+	@InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+	
+	@Mock
+    private MUserService userService;
+	
+	@Test
+	@DisplayName("")
+	void testLoadUserByUsername() {
+		//ダミーデータを宣言(selectUserDetail,loadUserByUsernameの引数用)
+		String testUserId = "testUserId";
+		String testPassword = "testPassword";
+		String testRole = "testRole";
+		//モックのMUserインスタンスの生成
+		MUser mockUser = new MUser();
+		//モックのMUserにダミーデータのセット
+		mockUser.setUserId(testUserId);
+		mockUser.setPassword(testPassword);
+		mockUser.setRole(testRole);
+		//userServiceのselectUserDetailメソッドを呼び出したときにモックのMUserを返すように設定
+		when(userService.selectUserDetail(testUserId)).thenReturn(mockUser);
+		//テスト対象のメソッドを実行してUserDetailsを受け取る
+		UserDetails resultUserDetails = userDetailsService.loadUserByUsername(testUserId);
+		//モックのMUserのuserIdが返されることを確認
+		assertEquals(mockUser.getUserId(), resultUserDetails.getUsername());
+		//モックのMUserのpasswordが返されることを確認
+		assertEquals(mockUser.getPassword(), resultUserDetails.getPassword());
+		boolean containsRoleUser = false;
+		for (GrantedAuthority authority : resultUserDetails.getAuthorities()) {
+		    if (authority.getAuthority().equals("testRole")) {
+		        containsRoleUser = true;
+		        break;
+		    }
+		}
+		//モックのMUserのroleがtrueであることを確認
+		assertTrue(containsRoleUser);
+	}
+
+
+}
