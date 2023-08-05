ee4078422dd1cfa28e82dd2ef3ab5007c1d01e88
Tsubakurouuu <yzpolice2896@icloud.com>
2023-08-05 16:29:47 +0900

    [Create]testRoundOff()メソッドの作成,test

--- src/test/java/com/example/controller/common/CommonControllerTest.java
+++ src/test/java/com/example/controller/common/CommonControllerTest.java
@@ -13,12 +13,12 @@ class CommonControllerTest {
 		//ダミーデータを宣言(calcWorkingOverメソッドの引数用)
         Integer testAttendanceHour = 9;
         Integer testAttendanceMinute = 20;
-        Integer teestLeavingHour = 20;
+        Integer testLeavingHour = 20;
         Integer testLeavingMinute = 50;
         Integer testRestHour = 1;
         Integer testRestMinute = 10;
-        //テスト対象のメソッドを実行してWorkを受け取る
-        Integer[] result = CommonController.calcWorkingOver(testAttendanceHour, testAttendanceMinute, teestLeavingHour, testLeavingMinute, testRestHour, testRestMinute);
+        //テスト対象のメソッドを実行してInteger[]を受け取る
+        Integer[] result = CommonController.calcWorkingOver(testAttendanceHour, testAttendanceMinute, testLeavingHour, testLeavingMinute, testRestHour, testRestMinute);
         //calcWorkingOverメソッドの期待する値
         Integer[] expected = { 10, 20, 2, 20 };
         //calcWorkingOverメソッドの実行結果と期待値が同値であることを確認
@@ -31,21 +31,58 @@ class CommonControllerTest {
 		//ダミーデータを宣言(calcWorkingOverメソッドの引数用)
         Integer testAttendanceHour = 9;
         Integer testAttendanceMinute = 20;
-        Integer teestLeavingHour = 17;
+        Integer testLeavingHour = 17;
         Integer testLeavingMinute = 50;
         Integer testRestHour = 1;
         Integer testRestMinute = 10;
-        //テスト対象のメソッドを実行してWorkを受け取る
-        Integer[] result = CommonController.calcWorkingOver(testAttendanceHour, testAttendanceMinute, teestLeavingHour, testLeavingMinute, testRestHour, testRestMinute);
-        // 期待値
+        //テスト対象のメソッドを実行してInteger[]を受け取る
+        Integer[] result = CommonController.calcWorkingOver(testAttendanceHour, testAttendanceMinute, testLeavingHour, testLeavingMinute, testRestHour, testRestMinute);
+        //calcWorkingOverメソッドの期待する値
         Integer[] expected = { 7, 20, 0, 0 };
-        // テストの実行
+        //calcWorkingOverメソッドの実行結果と期待値が同値であることを確認
         assertArrayEquals(expected, result);
 	}
 
 	@Test
-	void testRoundOff() {
-		fail("まだ実装されていません");
+	@DisplayName("5捨6入をして現在時分を返すメソッドのテスト1(分が切り捨てられるかのテスト)")
+	void testRoundOff1() {
+		//ダミーデータを宣言(roundOffメソッドの引数用)
+		Integer testHour = 5;
+		Integer testMinute = 23;
+		//テスト対象のメソッドを実行してInteger[]を受け取る
+		Integer[] result = CommonController.roundOff(testHour, testMinute);
+		//roundOffメソッドの期待する値
+		Integer[] expected = { 5, 20 };
+		//roundOffメソッドの実行結果と期待値が同値であることを確認
+		assertArrayEquals(expected, result);
+	}
+	
+	@Test
+	@DisplayName("5捨6入をして現在時分を返すメソッドのテスト2(分が切り上げられるかのテスト)")
+	void testRoundOff2() {
+		//ダミーデータを宣言(roundOffメソッドの引数用)
+		Integer testHour = 5;
+		Integer testMinute = 36;
+		//テスト対象のメソッドを実行してInteger[]を受け取る
+		Integer[] result = CommonController.roundOff(testHour, testMinute);
+		//roundOffメソッドの期待する値
+		Integer[] expected = { 5, 40 };
+		//roundOffメソッドの実行結果と期待値が同値であることを確認
+		assertArrayEquals(expected, result);
+	}
+	
+	@Test
+	@DisplayName("5捨6入をして現在時分を返すメソッドのテスト3(時が繰り上げられるかのテスト)")
+	void testRoundOff3() {
+		//ダミーデータを宣言(roundOffメソッドの引数用)
+		Integer testHour = 14;
+		Integer testMinute = 58;
+		//テスト対象のメソッドを実行してInteger[]を受け取る
+		Integer[] result = CommonController.roundOff(testHour, testMinute);
+		//roundOffメソッドの期待する値
+		Integer[] expected = { 15, 0 };
+		//roundOffメソッドの実行結果と期待値が同値であることを確認
+		assertArrayEquals(expected, result);
 	}
 
 	@Test
