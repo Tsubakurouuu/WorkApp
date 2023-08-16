0413612cde723fdcc0d3ab6e48567aaa7721b128
Tsubakurouuu <yzpolice2896@icloud.com>
2023-08-16 21:38:36 +0900

    [Create]【途中】testPostUserWorkAttendance1()メソッドの作成,test

--- src/test/java/com/example/controller/user/WorkInputControllerTest.java
+++ src/test/java/com/example/controller/user/WorkInputControllerTest.java
@@ -1,8 +1,14 @@
 package com.example.controller.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -11,6 +17,7 @@ import java.util.Calendar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,6 +26,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.config.AuthenticationSuccessHandler;
+import com.example.model.MUser;
+import com.example.model.Work;
 import com.example.service.WorkService;
 import com.example.service.impl.UserDetailsServiceImpl;
 
@@ -101,9 +110,49 @@ class WorkInputControllerTest {
 	}
 
 	@Test
-	@DisplayName("出勤ボタン押下時のメソッド")
-	void testPostUserWorkAttendance() {
-		fail("まだ実装されていません");
+	@DisplayName("出勤ボタン押下時のメソッド1(出勤打刻成功時)")
+	void testPostUserWorkAttendance1() throws Exception {
+		//モックのMUserを生成
+		MUser mockLoginUser = new MUser();
+		//モックのCalendarを生成
+		Calendar mockCalendar = Calendar.getInstance();
+		//ダミーデータを宣言(insertAttendanceメソッドの引数用)
+		Integer testUserId = 1;
+		Integer testYear = 2023;
+		Integer testMonth = 8;
+		Integer testDate = 15;
+		Integer testAttendanceHour = 10;
+		Integer testAttendanceMinute = 40;
+		//モックのMUser(mockLoginUser)にダミーデータのセット
+		mockLoginUser.setId(testUserId);
+		//userDetailsServiceImplのselectLoginUserメソッドが呼び出されたときにモックのMUserを返すように設定
+	    when(userDetailsServiceImpl.selectLoginUser()).thenReturn(mockLoginUser);
+		//mockMvcを使って"/work/attendance"にPOSTリクエストを送る
+	    mockMvc.perform(post("/work/attendance")
+	    		//HTTPリクエストのデータを設定
+	            .param("userId", testUserId.toString())
+	            .param("year", testYear.toString())
+	            .param("month", testMonth.toString())
+	            .param("date", testDate.toString())
+	            .param("attendanceHour", testAttendanceHour.toString())
+	            .param("attendanceMinute", testAttendanceMinute.toString()))
+	        //HTTPステータスが302（Found）であるとをを確認
+	        .andExpect(status().isFound())
+	        //"/work/input"へリダイレクトされることを確認
+	        .andExpect(redirectedUrl("/work/input"));
+	    //ArgumentCaptorの生成(Workの引数をキャッチするため)
+	    ArgumentCaptor<Work> argument = ArgumentCaptor.forClass(Work.class);
+	    //workServiceのinsertAttendanceが1度だけ呼び出されたことを確認
+	    verify(workService, times(1)).insertAttendance(argument.capture());
+	    //ArgumentCaptorでキャッチした引数の取得
+	    Work workArgument = argument.getValue();
+	    //insertAttendnceメソッドの実行結果と期待値が同値であることを確認
+	    assertEquals(testUserId, workArgument.getUserId());
+	    assertEquals(testYear, workArgument.getYear());
+	    assertEquals(testMonth, workArgument.getMonth());
+	    assertEquals(testDate, workArgument.getDate());
+	    assertEquals(testAttendanceHour, workArgument.getAttendanceHour());
+	    assertEquals(testAttendanceMinute, workArgument.getAttendanceMinute());
 	}
 
 	@Test
